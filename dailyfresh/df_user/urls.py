46abc8261e144996ba2b53e6ff24a448bd9bd072
wupeng <pengwu525@163.com>
2019-04-27 18:25:47 +0800

    完成用户基础注册功能

--- dailyfresh/df_user/urls.py
+++ dailyfresh/df_user/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from df_user.views import register
+from df_user.views import *
 
 urlpatterns = [
     url(r'^register/$', register),
+    url(r'^register_handle/$', register_handle),
 ]
--- dailyfresh/df_user/views.py
+++ dailyfresh/df_user/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from hashlib import sha1
+from df_user.models import UserInfo
 
 
 def register(request):
@@ -8,3 +10,38 @@ def register(request):
     :return:
     """
     return render(request, 'df_user/register.html')
+
+
+def register_handle(request):
+    """
+    注册处理页面
+    """
+    # 从request获取参数信息
+    post = request.POST
+    uname = post.get('user_name')
+    upwd = post.get('pwd')
+    upwd2 = post.get('cpwd')
+    uemail = post.get('email')
+
+    # 判断两次密码是否相同
+    # 如果不同, 重定向到registe请求
+    if upwd != upwd2:
+        return redirect('/user/register/') 
+
+    # 如果相同
+    # 1. 对密码进行加密
+    s1 = sha1()
+    s1.update(upwd.encode('utf-8'))
+    upwd3 = s1.hexdigest()
+
+    # 2. 创建user模型对象
+    user = UserInfo()
+    user.uname = uname
+    user.upwd = upwd3
+    user.uemail = uemail
+
+    # 3. 保存模型对象
+    user.save()
+
+    # 4. 跳转到登录页面
+    return redirect('/user/login/')
