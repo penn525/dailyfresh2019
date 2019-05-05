785c563dc0308d791d009ed328cf16c55e94e2e3
wupeng <pengwu525@163.com>
2019-05-05 17:22:07 +0800

    完成首页面基本商品的展示

--- dailyfresh/df_goods/models.py
+++ dailyfresh/df_goods/models.py
@@ -16,11 +16,11 @@ class GoodsInfo(models.Model):
     商品模型
     """
     gtitle = models.CharField(u"名称", max_length=20)
-    gprice = models.DecimalField(u"单价", max_digits=5, decimal_places=2)
+    gprice = models.DecimalField(u"单价", max_digits=5, decimal_places=2, default=50.10)
     gpicture = models.ImageField(u"图片", upload_to='df_goods', height_field=None, width_field=None, max_length=None)
     isDelete = models.BooleanField(u"是否删除", default=False)
     gunit = models.CharField(u"计量单位", max_length=20, default='500g')
-    gintroduction = models.CharField(u"简介", max_length=100)
+    gintroduction = models.CharField(u"简介", max_length=100, default='无简介')
     gdetail = HTMLField() # 富文本编辑器
     gstock = models.IntegerField(u"库存量", default=500)
     gclick = models.IntegerField(u"点击量", default=0)
--- dailyfresh/df_goods/views.py
+++ dailyfresh/df_goods/views.py
@@ -1,11 +1,41 @@
 from django.shortcuts import render
 from django.urls import reverse
+from df_goods.models import TypeInfo, GoodsInfo
 
 def index(request):
     """
     商品首页
     """
-    context = {'guster_page': 1, 'title': '首页'}
+    # 1. 获取所有分类
+    type_list = TypeInfo.objects.all()
+    # 2. 获取每个分类的前4个商品
+    goods0 = type_list[0].goodsinfo_set
+    goods0_id = goods0.order_by('-id')[0:4]
+    goods0_click = goods0.order_by('-gclick')[0:4]
+    goods1 = type_list[1].goodsinfo_set
+    goods1_id = goods1.order_by('-id')[0:4]
+    goods1_click = goods1.order_by('-gclick')[0:4]
+    goods2 = type_list[2].goodsinfo_set
+    goods2_id = goods2.order_by('-id')[0:4]
+    goods2_click = goods2.order_by('-gclick')[0:4]
+    goods3 = type_list[3].goodsinfo_set
+    goods3_id = goods3.order_by('-id')[0:4]
+    goods3_click = goods3.order_by('-gclick')[0:4]
+    goods4 = type_list[4].goodsinfo_set
+    goods4_id = goods4.order_by('-id')[0:4]
+    goods4_click = goods4.order_by('-gclick')[0:4]
+    goods5 = type_list[5].goodsinfo_set
+    goods5_id = goods5.order_by('-id')[0:4]
+    goods5_click = goods5.order_by('-gclick')[0:4]
+
+    print(goods0_id)
+    context = {'guster_page': 1, 'title': '首页', 'types': type_list, 
+        'goods0_id': goods0_id, 'goods0_click': goods0_click,
+        'goods1_id': goods1_id, 'goods1_click': goods1_click,
+        'goods2_id': goods2_id, 'goods2_click': goods2_click,
+        'goods3_id': goods3_id, 'goods3_click': goods3_click,
+        'goods4_id': goods4_id, 'goods4_click': goods4_click,
+        'goods5_id': goods5_id, 'goods5_click': goods5_click,}
     return render(request, 'df_goods/index.html', context)
 
 
